58226be45be0c4b4b8a0719cb0939dd841e56783
alizul01 <alizulfikar032@gmail.com>
2024-06-19 09:23:33 +0700

    add: weapon projectile

---
+++ Assets/Scripts/Character/Abilities/CharacterShoot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Character.Abilities
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class CharacterShoot: MonoBehaviour
+    {
+        public GameObject projectilePrefab;
+        public float projectileSpeed;
+
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 spawnPosition = transform.position + transform.forward;
+                GameObject projectile = Instantiate(projectilePrefab, spawnPosition, Quaternion.identity);
+                Vector3 projectileDirection = transform.forward;
+                
+                _rb.AddForce(projectileDirection * projectileSpeed, ForceMode.Impulse);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                Debug.Log("Nyawa enemy berkurang");
+            }
+        }
+    }
+}
